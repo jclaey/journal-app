6569513e8d588851256fef94bb123559a1cfa43a
Jason <jdclaeys@outlook.com>
2021-06-14 20:38:53 -0500

    add basic validation to register user route

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
+const User = require('../models/User');
 
-router.post('/', (req, res) => {
-  res.send('Register a user');
+router.post('/', [
+  body('name', 'Please provide a name')
+    .not()
+    .isEmpty(),
+  body('email', 'Please include a valid email')
+    .isEmail(),
+  body('password', 'Please enter a password with 6 or more characters')
+    .isLength({ min: 6 })
+], (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 });
 
 module.exports = router;
