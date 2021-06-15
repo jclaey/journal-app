32ffe92284b3ae0f34c5bf1566b78a2f21f4cbf8
Jason <jdclaeys@outlook.com>
2021-06-14 21:18:10 -0500

    add register user functionality with password encryption

--- routes/users.js
+++ routes/users.js
@@ -17,6 +17,33 @@ router.post('/', [
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
+
+  const { name, email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({ msg: 'A user with that email already exists' });
+    }
+
+    user = new User({
+      name,
+      email,
+      password
+    });
+
+    const salt = await bcrypt.genSalt(10);
+
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+
+    res.send('User saved');
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 module.exports = router;
