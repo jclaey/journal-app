d77b0667181b30491203dfd70f67812cd0b283fe
Jason <jdclaeys@outlook.com>
2021-06-18 22:15:28 -0500

    set up initial React front-end

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Journy</h1>
+      <p className="my-1">Journy is a web application that allows users to create private journal entries for their eyes and enjoyment only or public journal entries imparted to the entire world. Sign up today and start sharing your Journy.</p>
+    </div>
+  )
+};
+
+export default About;
